5d1874984ec4d9849b4d3c001a33f22689838c31
gotZ9 <353127981@qq.com>
2018-05-23 19:32:16 +0800

    第一次提交

---
+++ c1/src/helloWorld.erl
@@ -0,0 +1,17 @@
+%%%-------------------------------------------------------------------
+%%% @author Administrator
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 23. 五月 2018 19:17
+%%%-------------------------------------------------------------------
+-module(helloWorld).
+-author("Administrator").
+
+%% API
+-export([main/0]).
+
+
+main() ->
+     "helloWorld".
